9cbecb9681debd3b4ecabf0a9f555f941232a41f
Michal G <miki.glodek@wp.pl>
2021-04-25 17:35:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flight and airport information API
+
+##### Task made for smart4aviation recruitment process 
---
+++ src/main/java/com/example/flightapi/FlightInformationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.flightapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInformationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightInformationApiApplication.class, args);
+    }
+
+}
