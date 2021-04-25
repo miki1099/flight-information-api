1a82cedb90311f6f4656905aa848dc2b9518fc6f
Michal G <miki.glodek@wp.pl>
2021-04-25 22:27:01 +0200

    add global controller advice

---
+++ src/main/java/com/example/flightapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.flightapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {FlightNotFoundException.class})
+    protected ResponseEntity<Object> handleUserNotFound(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
+        return new ResponseEntity<>(bodyOfResponse, HttpStatus.NOT_FOUND);
+    }
+}
