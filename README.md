ba816f6ec1f0e392e580d7c74ac227fc5b1ea984
Michal G <miki.glodek@wp.pl>
2021-04-26 18:32:54 +0200

    add swagger description, update readme, add ssytem properties for heroku deploy

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Flight and airport information API
 
 ##### Task made for smart4aviation recruitment process 
+
+#####You can test Api by swagger on:
+
+https://flight-information-api.com/swagger-ui/index.html#/
+
+## available API
+- get
+    - Get flight and baggage numbers in airport
+    - Get weight of specific flight
+- post
+    - Create new flight
+    - Create cargo for flight
+    
+## example request
+
+##### curl:
+    curl -X GET "https://flight-information-api.com/airport/getFlightAndBaggageNumbers/GDN" -H "accept: */*"
+    
+##### response body:
+
+    {
+      "departingFlightsNumber": 1,
+      "arrivingFlightsNumber": 2,
+      "baggageArrivingPieces": 2338,
+      "baggageDepartingPieces": 2751
+    }
--- src/main/java/com/example/flightapi/config/SwaggerConfig.java
+++ src/main/java/com/example/flightapi/config/SwaggerConfig.java
@@ -1,8 +1,5 @@
 package com.example.flightapi.config;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -23,13 +20,14 @@ public class SwaggerConfig {
     public Docket swaggerSpringfoxDocket() {
         Contact contact = new Contact(
                 "Michał Głodek",
-                "https://github.com/miki1099/learning-application-with-shop",
+                "https://github.com/miki1099/flight-information-api",
                 "miki.glodek@wp.pl");
 
         List<VendorExtension> vext = new ArrayList<>();
         ApiInfo apiInfo = new ApiInfo(
                 "recruitment task API",
-                "",
+                "Please be patient db is free and after some inactivity time it take some time to load data! \n" +
+                        "Example data can be found in github (Michał Głodek - Website) resources/sata.sql or you can add your own.",
                 "0.0.1",
                 "",
                 contact,
@@ -38,6 +36,7 @@ public class SwaggerConfig {
                 vext);
 
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
